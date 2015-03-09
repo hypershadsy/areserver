87d565d4b7ad6f5ca318ff8b221d4cded8461abf
Hypershadsy <hypershadsy@gmail.com>
2015-03-09 01:25:08 -0400

    Wall meta

--- Areserver/Wall.cs
+++ Areserver/Wall.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Reflection;
 
 namespace Areserver
 {
@@ -6,12 +7,53 @@ namespace Areserver
     {
         public bool LeftFacing { get; set; }
 
+        public int WallID
+        {
+            get
+            {
+                Attribute attr = Attribute.GetCustomAttribute(this.GetType(), typeof(WallMetaAttribute));
+                int id = ((WallMetaAttribute)attr).Id;
+                return id;
+            }
+        }
+
+        /// <summary>
+        /// Makes a new wall whose type is specified by id.
+        /// </summary>
+        /// <returns>A new subclass of Wall with default params.</returns>
+        /// <param name="id">Integer identifier associated with the block type.</param>
+        public static Wall ConstructFromID(int id, bool leftFacing)
+        {
+            Assembly thisAsm = Assembly.GetExecutingAssembly();
+            foreach (Type t in thisAsm.GetTypes())
+            {
+                //get this type's attributes
+                object[] attrs = t.GetCustomAttributes(typeof(WallMetaAttribute), false);
+                foreach (var attr in attrs)
+                {
+                    if (attr.GetType() == typeof(WallMetaAttribute))
+                    {
+                        int thisTypesId = ((WallMetaAttribute)attr).Id;
+                        if (thisTypesId == id)
+                        {
+                            //we've found the blocktype, now construct
+                            ConstructorInfo ctor = t.GetConstructor(new Type[] { });
+                            object newObj = ctor.Invoke(new object[] { leftFacing });
+                            return (Wall)newObj;
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
         public Wall(bool leftFacing)
         {
             this.LeftFacing = leftFacing;
         }
     }
 
+    [WallMetaAttribute(0)]
     class RedBrickWall : Wall
     {
         public RedBrickWall(bool leftFacing)
