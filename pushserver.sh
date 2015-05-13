526734eb1ee7e8e01a544f40b7c2682fdb17762a
Hypershadsy <hypershadsy@gmail.com>
2015-05-13 19:17:56 -0400

    cleanups

--- pushserver.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-FILES="Areserver/bin/Debug/Areserver.exe Areserver/bin/Debug/Lidgren.Network.dll"
-
-rsync -avz -e 'ssh -p 2222' --progress ${FILES} Theater@giga.krash.net:/cygdrive/c/Users/Theater/Desktop/jared/areserver
